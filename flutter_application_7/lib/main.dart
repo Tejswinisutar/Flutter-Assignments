c164ffc5b3e16c07a01af47c90f9ecf6a38f32a4
Tejswinisutar <tejswinisutar56@gmail.com>
2025-05-14 20:30:04 +0530

    Assignment

---
+++ flutter_application_7/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String str = "Assignment No 6";
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title:  Text(str),backgroundColor: Colors.purple),
+        body: Align(
+          alignment: Alignment.centerRight,
+           child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.pinkAccent,
+           ),
+        ),
+      ),
+    );
+  }
+}
